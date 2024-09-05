ad2048df6247bdbdb4b9bb85389bce257093ad9a
Matt Davis <matt.davis@alleninstitute.org>
2024-09-05 21:32:53 +0000

    env update

---
+++ code/file_handling.py
@@ -0,0 +1,153 @@
+## copied from comb
+
+from pathlib import Path
+import json
+from typing import Union
+
+# set up logger
+import logging
+logger = logging.getLogger(__name__)
+
+
+
+MULTIPLANE_FILE_PARTS = {"processing_json": "processing.json",
+                           "params_json": "_params.json",
+                           "registered_metrics_json": "_registered_metrics.json",
+                           "average_projection_png": "_average_projection.png",
+                           "max_projection_png": "_maximum_projection.png",
+                           "motion_transform_csv": "_motion_transform.csv",
+                           "segmentation_output_json": "segmentation_output.json",
+                           "roi_traces_h5": "roi_traces.h5",
+                           "neuropil_correction_h5": "neuropil_correction.h5",
+                           "neuropil_masks_json": "neuropil_masks.json",
+                           "neuropil_trace_output_json": "neuropil_trace_output.json",
+                           #"demixing_h5": "demixing_output.h5",
+                           #"demixing_json": "demixing_output.json",
+                           "dff_h5": "dff.h5",
+                           "extract_traces_json": "extract_traces.json",
+                           "events_oasis_h5": "events_oasis.h5"}
+
+def multiplane_session_data_files(input_path):
+    """Find all data files in a multiplane session directory."""
+    input_path = Path(input_path)
+    data_files = {}
+    for key, value in MULTIPLANE_FILE_PARTS.items():
+        data_files[key] = find_data_file(input_path, value)
+    return data_files
+
+
+def find_data_file(input_path, file_part, verbose=False):
+    """Find a file in a directory given a partial file name.
+
+    Example
+    -------
+    input_path = /root/capsule/data/multiplane-ophys_724567_2024-05-20_12-00-21
+    file_part = "_sync.h5"
+    return: "/root/capsule/data/multiplane-ophys_724567_2024-05-20_12-00-21/ophys/1367710111_sync.h5"
+    
+    
+    Parameters
+    ----------
+    input_path : str or Path
+        The path to the directory to search.
+    file_part : str
+        The partial file name to search for.
+    """
+    input_path = Path(input_path)
+    try:
+        file = list(input_path.glob(f'**/*{file_part}*'))[0]
+    except IndexError:
+        if verbose:
+            logger.warning(f"File with '{file_part}' not found in {input_path}")
+        file = None
+    return file
+
+
+def get_file_paths_dict(file_parts_dict, input_path):
+    file_paths = {}
+    for key, value in file_parts_dict.items():
+        file_paths[key] = find_data_file(input_path, value)
+    return file_paths
+
+
+def check_ophys_folder(path):
+    """ophys folders can have multiple names, check for all of them"""
+    ophys_names = ['ophys', 'pophys', 'mpophys']
+    ophys_folder = None
+    for ophys_name in ophys_names:
+        ophys_folder = path / ophys_name
+        if ophys_folder.exists():
+            break
+        else:
+            ophys_folder = None
+
+    return ophys_folder
+
+
+def check_behavior_folder(path):
+    behavior_names = ['behavior', 'behavior_videos']
+    behavior_folder = None
+    for behavior_name in behavior_names:
+        behavior_folder = path / behavior_name
+        if behavior_folder.exists():
+            break
+        else:
+            behavior_folder = None
+    return behavior_folder
+
+
+def get_sync_file_path(input_path, verbose=False):
+    """Find the Sync file"""
+    file_parts = {}
+    input_path = Path(input_path)
+    try: 
+        # method 1: find sync_file by name
+        file_parts = {"sync_h5": "_sync.h5"}
+        sync_file_path = find_data_file(input_path, file_parts["sync_h5"], verbose=False)
+    except IndexError as e:
+        if verbose:
+            logger.info("file with '*_sync.h5' not found, trying platform json")
+
+    if sync_file_path is None:
+        # method 2: load platform json
+        # Note: sometimes fails if platform json has incorrect sync_file path
+        logging.info(f"Trying to find sync file using platform json for {input_path}")
+        file_parts = {"platform_json": "_platform.json"}
+        platform_path = find_data_file(input_path, file_parts["platform_json"])
+        with open(platform_path, 'r') as f:
+            platform_json = json.load(f)
+        ophys_folder = check_ophys_folder(input_path)
+        sync_file_path = ophys_folder / platform_json['sync_file']
+
+        if not sync_file_path.exists():
+            logger.error(f"Unsupported data asset structure, sync file not found in {sync_file_path}")
+            sync_file_path = None
+        else:
+            logger.info(f"Sync file found in {sync_file_path}")
+
+    return sync_file_path
+
+
+def plane_paths_from_session(session_path: Union[Path, str],
+                             data_level: str = "raw") -> list:
+    """Get plane paths from a session directory
+
+    Parameters
+    ----------
+    session_path : Union[Path, str]
+        Path to the session directory
+    data_level : str, optional
+        Data level, by default "raw". Options: "raw", "processed"
+
+    Returns
+    -------
+    list
+        List of plane paths
+    """
+    session_path = Path(session_path)
+    if data_level == "processed":
+        planes = [x for x in session_path.iterdir() if x.is_dir()]
+        planes = [x for x in planes if 'nextflow' not in x.name]
+    elif data_level == "raw":
+        planes = list((session_path / 'ophys').glob('ophys_experiment_*'))
+    return planes
