a556f3eea247c9fc7e4f3abad05fdd116b60ff08
Ahad-Allen <ahad.bawany@alleninstitute.org>
2025-06-27 14:59:26 +0000

    We want the capsule folder, not behavior lol

--- code/file_handling.py
+++ code/file_handling.py
@@ -50,7 +50,7 @@ def singleplane_session_data_files(input_path: Union[str, Path]):
         singleplane_data_path = input_path
 
     for subfolder in subfolders:
-        if "behavior_nwb" in subfolder.name:
+        if "capsule" in subfolder.name:
             singleplane_data_path = subfolder
 
     # This is the actual data directory
